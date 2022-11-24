7fa19e602edcf3a7f2705dd0e788f212a0ac6a7c
ngbinetou <binetoucecile20022@gmail.com>
2022-11-25 00:17:52 +0100

    MiniProjet

--- solutions/ses3/Readlines.c
+++ solutions/ses3/Readlines.c
@@ -9,52 +9,202 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#define SIZE_BUFF 8
+#define SIZE_BUFF 66
 
 
 
-char* readlines (int fd)
+
+//fonction pour la longueur de la chaine
+int strlong(char* str)
 {
+	int	n=0;
 
-	char* buf= malloc(sizeof(char));
-
-	static char *tmp= malloc(SIZE_BUFF*(sizeof(char)));
-
-	while()
+	if (str)
 	{
-		r=read(fd, buf, SIZE_BUFF);
+		while (str[n] != '\0')
+			n=n+1;
+		return (n);
+	}
+	return (1);
+}
 
-		for(int i=0; i<SIZE_BUFF;i++)
+
+
+
+
+
+
+// fonction pour chercher un caractere dans un mot
+char* strchrch(char *str, char c)
+{
+	int	n=0;
+
+	
+	while (str[n] != '\0')
+	{
+		if (str[n] == c)
+			return (&str[n]);
+		n=n+1;
+	}
+	return (0);
+}
+
+
+
+
+
+
+
+//fonction pour concatener 2 chaines
+char* strconcat(char* c1, char* c2)
+{
+	int		i=0;
+	char	*str;
+	char	*r;
+
+	
+	if (c1== NULL || c2== NULL)
+		return (0);
+
+	char* c3 = (char*) malloc((strlong(c1)+strlong(c2)+1)*sizeof(char));
+
+
+	for(i=0; c1[i]!='\0'; i++) 
+		c3[i] = c1[i];
+
+	i=0;
+	for(i=0; c2[i]!='\0'; i++) 
+		c3[strlong(c1)+i] = c2[i];
+
+	c3[(strlong(c1)+strlong(c2)+1)]='\0';
+
+
+	return (0);
+}
+
+
+
+
+
+
+// la fonction read_lines
+char* read_lines (int fd)
+{
+	int i;
+	int j;
+	static char* tmp1;
+	static char* tmp;
+	tmp = (char*)malloc(SIZE_BUFF*(sizeof(char)));
+	static int nbCalls;
+
+	char* buf= malloc(SIZE_BUFF*(sizeof(char)));
+
+	
+
+	if(nbCalls && strchrch(tmp,'\n'))
+	{
+		for (i=0;i<strlong(tmp);i++)
 		{
-			if(strch(r,'\n'))
+			while (tmp[i]!='\n')
+			{
+				j+=1;
+				i+=1;
+			}
+			int k=j;
+
+			if (j=(strlong(tmp)-1))
+			
+				return tmp;
+				
+			if (j<(strlong(tmp)-1))
+			{
+				char* tmpp= malloc(j*(sizeof(char)));
+				for (i=0; i<j;i++)
+				{
+					
+					tmpp[i]=tmp[k];
+					k++;
+					i+=1;
+				}
+				return tmpp;
+
+	
+				tmp1= malloc((strlong(tmp)-(j+1)+1)); //+1 for (\0)*(sizeof(char))) everywhere
+
+				for(i=j+1;i<strlong(tmp);i++)
+				{
+					tmp1[i-(j+1)]=tmp[i];
+					i+=1;
+				}
+				tmp1;
+				free(tmp);
+			
+				free (tmpp);
+			}		
+		}
+				
+	}
+
+	
+
+	else
+	{
+		
+		int r=read(fd, buf, SIZE_BUFF);
+		
+
+		for (i=0; i<SIZE_BUFF;i++)
+		{
+			if(strchrch(buf,'\n'))
 			{
 				while(i+1<SIZE_BUFF)
 				{
-					tmp+=r[i+1];
+					int k=0;
+					tmp[k]=buf[i+1];
 					i+=1;
+					k+=1;		
 				}
 			}
-				EXIT()
-
+			else
+			printf("%s",buf);
+	
 		}
-		
-		
-	}
+	}			
+	
+	nbCalls==1;
 
-	return 
+	return tmp1;
 
 }
+
 
 
 
 
 
 int main()
-{
+{		
 
-	int fd = open("/home/cecile/bureau/test",664);
+		//si read=0 donc fichier est vide
+		//concatener tmp1 avec le nouveau r pour obtenir le nouveau tmp;
+		//tmp=strconcat(tmp1,r);
+
+	
+	int fd = open("/home/cecile/Documents/LSD2/test",664);
+	char* buf= malloc(SIZE_BUFF*(sizeof(char)));
+	//static int r;
+	int r=read(fd, buf, SIZE_BUFF);
+
+	While(r!=0)
+	{
+		char* k=read_lines(fd);
+		tmp= strconcat(tmp1,r);
+	}
 
 
 
 	return 0;
 }
+
+
+
